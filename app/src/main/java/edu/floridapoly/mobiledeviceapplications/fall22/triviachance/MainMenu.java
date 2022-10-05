c5e33562ca441e84c3d4f82f6a45d53456b9f042
Greg <gtj0806@gmail.com>
2022-10-04 21:23:14 -0400

    *Added basic buttons for a skeliton main menu *Added activities for inventory and a basic question screen

--- app/src/main/java/edu/floridapoly/mobiledeviceapplications/fall22/triviachance/MainMenu.java
+++ app/src/main/java/edu/floridapoly/mobiledeviceapplications/fall22/triviachance/MainMenu.java
@@ -1,14 +1,83 @@
 package edu.floridapoly.mobiledeviceapplications.fall22.triviachance;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
+
+    Button playOnline;
+    Button playSolo;
+    Button settings;
+    Button inventory;
+
+/*
+    ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+
+                }
+            }
+    );
+
+ */
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+
+
+        playOnline = findViewById(R.id.play_online);
+        playOnline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        playSolo = findViewById(R.id.play_solo);
+        playSolo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainMenu.this, QuestionActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        settings = findViewById(R.id.settings);
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        inventory = findViewById(R.id.inventory);
+        inventory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainMenu.this, InventoryActivity.class);
+                // taken out temporarily
+               // activityResultLauncher.launch(intent);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+
 }
---
+++ app/src/main/java/edu/floridapoly/mobiledeviceapplications/fall22/triviachance/QuestionActivity.java
@@ -0,0 +1,14 @@
+package edu.floridapoly.mobiledeviceapplications.fall22.triviachance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+    }
+}
