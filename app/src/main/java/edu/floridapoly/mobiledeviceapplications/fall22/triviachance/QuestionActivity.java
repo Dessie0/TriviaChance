4fc200bc1078ad8678355651dc856046b002515b
Greg <gtj0806@gmail.com>
2022-10-13 18:05:18 -0400

    Added multiple questions (currently hardcoded) Made basic results screen *NOTE: from results screen, press back arrow do not use phone's back button

--- app/src/main/java/edu/floridapoly/mobiledeviceapplications/fall22/triviachance/QuestionActivity.java
+++ app/src/main/java/edu/floridapoly/mobiledeviceapplications/fall22/triviachance/QuestionActivity.java
@@ -2,9 +2,11 @@ package edu.floridapoly.mobiledeviceapplications.fall22.triviachance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,7 +18,14 @@ public class QuestionActivity extends AppCompatActivity {
     Button answer2;
     Button answer3;
     Button answer4;
+    Button[] answerButtons;
     TextView questionText;
+    String[] questions;
+    String[][] answers;
+
+    int currentQuestionIndex = 0;
+    int numberCorrect;
+    int numberWrong;
 
 
     @Override
@@ -25,35 +34,66 @@ public class QuestionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_question);
 
         questionText = findViewById(R.id.questionTextView);
-        questionText.setText(R.string.question);
+
 
         answer1 = findViewById(R.id.answer1);
         answer2 = findViewById(R.id.answer2);
         answer3 = findViewById(R.id.answer3);
         answer4 = findViewById(R.id.answer4);
 
-        Button answerButtons[] = {answer1, answer2, answer3, answer4};
+        answerButtons = new Button[]{answer1, answer2, answer3, answer4};
 
         // right now has hardcoded strings
         // need a new way to get 4 answer choices
-        String answers[] = getResources().getStringArray(R.array.answers);
+        answers = new String[][]{
+                {"Squirrel", "Bat", "Phoenix", "Alligator"},
+                {"Mars", "Earth", "Jupyter", "Venus"},
+                {"bird alert", "tweet me", "twttr", "sweet tweet"},
+                {"Fe", "Na", "K", "H"}};
+        questions = getResources().getStringArray(R.array.questions);
+
+        initQuestion(currentQuestionIndex);
+    }
+
+    public void initQuestion(int index) {
+        for (int i = 0; i < 4; i++) {
+            answerButtons[i].setBackgroundColor(getResources().getColor(R.color.purple_500));
+        }
+        questionText.setText(questions[index]);
 
         for (int i = 0; i < 4; i++) {
-            answerButtons[i].setText(answers[i]);
+            answerButtons[i].setText(answers[index][i]);
         }
     }
 
     public void onClickAnswer(View view) {
         if (view.getId() == answer3.getId()) {
-            Toast.makeText(this, "Correct", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(this, "Correct", Toast.LENGTH_SHORT).show();
             view.setBackgroundColor(Color.GREEN);
+            numberCorrect++;
         }
         else {
-            Toast.makeText(this, "Wrong", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(this, "Wrong", Toast.LENGTH_SHORT).show();
             view.setBackgroundColor(Color.RED);
+            numberWrong++;
         }
 
-        Intent intent = new Intent(QuestionActivity.this, ResultsActivity.class);
-        startActivity(intent);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (currentQuestionIndex < 3) {
+                    initQuestion(++currentQuestionIndex);
+                }
+                else {
+                    Intent intent = new Intent(QuestionActivity.this, ResultsActivity.class);
+                    intent.putExtra("CORRECT", numberCorrect);
+                    intent.putExtra("INCORRECT", numberWrong);
+                    startActivity(intent);
+                }
+            }
+        }, 500);
+
+
+
     }
 }
--- app/src/main/java/edu/floridapoly/mobiledeviceapplications/fall22/triviachance/ResultsActivity.java
+++ app/src/main/java/edu/floridapoly/mobiledeviceapplications/fall22/triviachance/ResultsActivity.java
@@ -2,13 +2,30 @@ package edu.floridapoly.mobiledeviceapplications.fall22.triviachance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class ResultsActivity extends AppCompatActivity {
+
+    TextView numCorrectText;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+
+        Intent intent = getIntent();
+        int correct = intent.getIntExtra("CORRECT", 0);
+
+        numCorrectText = findViewById(R.id.numCorrectText);
+        numCorrectText.setText(String.format("You got %d correct", correct));
+
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setProgress(correct * 25);
+
     }
 }
